c2193c8ed14316ce8f47d2112d01eaa7abb8617a
kingsley2emelideme <kingsley.emelideme@hotmail.com>
2018-09-01 22:09:00 -0600

    Added internet connection script.

---
+++ internet_check.py
@@ -0,0 +1,12 @@
+import requests
+
+
+def check_internet():
+    url = 'http://www.google.com/'
+    timeout = 5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        print("İnternet bağlantısı yok.")
+    return False
